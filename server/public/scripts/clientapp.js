8a9359efda9bd39963be6243166bbcf83f464019
nah <2trill2spill@gmail.com>
2016-08-26 09:57:01 -0500

    Routing exercise.

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -2,15 +2,31 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-    .when('/home', {
-      templateUrl: '/views/home.html',
-      controller: "HomeController"
+    .when('/kirby', {
+      templateUrl: '/views/templates/kirby.html',
+      controller: "kirbyController"
     })
-    .when('/orders', {
-      templateUrl: '/views/orders.html',
-      controller: "OrdersController"
+    .when('/link', {
+      templateUrl: '/views/templates/link.html',
+      controller: "linkController"
+    })
+    .when('/lupin', {
+      templateUrl: '/views/templates/lupin.html',
+      controller: "lupinController"
     })
     .otherwise({
-      redirectTo: 'home'
+      redirectTo: '/lupin'
     })
+}]);
+
+myApp.controller("kirbyController", ["$scope", function($scope){
+    console.log("Kirby");
+}]);
+
+myApp.controller("linkController", ["$scope", function($scope){
+    console.log("Link");
+}]);
+
+myApp.controller("lupinController", ["$scope", function($scope){
+    console.log("Lupin");
 }]);
